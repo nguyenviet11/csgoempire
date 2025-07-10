ff8ebc3fe19bd1374a83444a39e3162029af0cda
vietnv-mmo <nguyenvanvietbn1998@gmail.com>
2025-07-10 10:45:45 +0700

    ss

--- src/app/page.js
+++ src/app/page.js
@@ -15,7 +15,7 @@ export default function Home() {
     >
       <main className={styles.main}>
         <h3 className={styles.text}>
-        CSGOEmpire Official Site{" "}
+        SciSpace Official Site{" "}
           <span
             style={{ transform: "translate(0px)", display: "inline-block" }}
           >
@@ -27,7 +27,7 @@ export default function Home() {
         </p>
         <p className={styles.text3}>
           We needs to review the security of your connection before proceeding
-          CSGOEmpire
+          SciSpace
         </p>
        
         <div style={{ marginTop: 30, position: "relative" }}>
